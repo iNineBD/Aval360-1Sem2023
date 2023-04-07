3bcefbe13b7d0ed96c03c1e328c193f3ffa9ab84
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-04-07 14:25:13 -0300

    Finalização da função Editar turmas

--- prompt.py
+++ prompt.py
@@ -6,17 +6,19 @@ while True:
     print('')
     print("Escolha uma opção: \n1 - Criar nova turma\n2 - Visualizar turmas\n3 - Editar turma\n4 - Controle de times\n0 - Voltar\n")
     op = int(input("Digite aqui: "))
+    
     if op == 1:
-        ident = input("\nEntre com o nome da turma: ")
-        print(Turmas.createTurmas(ident))
+        print("\nCriar nova turma!!!\n")
+        print(Turmas.createTurmas())
+        
     elif op == 2:
-        turmas = Turmas.getNameAllTurmas()
-        x = 1
-        for name in turmas:
-            print(f"{x} - {name}")
-            x = x+1
+        print("\nVisualizando turmas!!!\n")
+        Turmas.listAllTurmas()
+        
     elif op == 3:
-        pass
+        print("\nEditar turma!!!\n")
+        print(Turmas.editTurmas())
+        
     elif op == 4:
         pass
     elif op == 0:
