0825748dfbff3217fb402b58bd79d4908cd29cde
Beatriz <125521054+BeatrizBonatto@users.noreply.github.com>
2023-04-11 19:10:11 -0300

    Versão 2

--- ctr_ed_times.py
+++ ctr_ed_times.py
@@ -1,32 +1,51 @@
 import json
 
-arqv = open('C:/Users/biake/Downloads/times.json')
-read_arqv = json.load(arqv) #.load() - leitura do arqvo
+arqv_turma = open('C:/Users/biake/Downloads/turmas.json')
+ler_arqv_turma = json.load(arqv_turma)
+
+print('\nTurmas:')
+
+for turma in ler_arqv_turma:
+    print(turma.get('id_turma'), '-', turma.get('identificacao'))
+
+id_turma = str(input('\nDigite qual turma deseja visualizar os times: '))
+
+arqv_time = open('C:/Users/biake/Downloads/times.json')
+ler_arqv_time = json.load(arqv_time)  # .load() - leitura do arqvo
 
 print('\nTimes:')
 
-for time in read_arqv:
-    print(time.get('id_time'), '-', time.get('identificacao'))
+for time in ler_arqv_time:
 
-num_time = str(input('\nDigite qual time deseja editar: '))
-print('\nInsira as novas informações\n')
-novo_nome_time = input('Nome do time: ')
-novo_num_intg = input('Número máximo de integrantes: ')
-
-for time in read_arqv:
-    if time.get('id_time') == num_time: #get() - metododo p/ puxar o valor que eu quero
-        time['identificacao'] = novo_nome_time
-        time['max'] = novo_num_intg
-        print('\n-----------------------------')
+    if str(time.get('id_turma')) == id_turma:
         print(time.get('id_time'), '-', time.get('identificacao'))
-        print('Máximo de integrantes: ', time.get('max'))
-        print('Turma: ', time.get('id_turma'))
-        print('-----------------------------')
-        print('\nEdição realizada com sucesso!')
+
+        id_time = str(input('\nDigite qual time deseja editar: '))
+
+    # elif time.get('id_turma') == False:
+    #     print('Turma inválida!')
+
+    for time in ler_arqv_time:
+        if time.get('id_time') == id_time:  # get() - metododo p/ puxar o valor que eu quero
+            print('\nInsira as novas informações\n')
+            novo_nome_time = input('Nome do time: ')
+            novo_num_intg = input('Número máximo de integrantes: ')
+
+            time['identificacao'] = novo_nome_time
+            time['max'] = novo_num_intg
+            print('\n--------------------------')
+            print(time.get('id_time'), '-', time.get('identificacao'))
+            print('Máximo de integrantes: ', time.get('max'))
+            print('Turma: ', time.get('id_turma'))
+            print('--------------------------')
+            print('\nEdição realizada com sucesso!')
+
+        # elif time.get('id_time') :
+        #     print('Time inválido!')
 
     caminho = 'C:/Users/biake/Downloads/times.json'
 
     with open(caminho, 'w') as output:
-        json.dump(read_arqv, output)
+        json.dump(ler_arqv_time, output)
 
 output.close()
