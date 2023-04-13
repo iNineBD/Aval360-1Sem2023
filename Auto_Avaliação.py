c15a1f31d397aee875426fff6e94f0f04fc34f5c
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2023-04-13 03:05:28 -0300

    Erros blocos e formatação

--- Auto_Avaliação.py
+++ Auto_Avaliação.py
@@ -7,52 +7,44 @@ local_resposta = 'data/respostas.json'
 y = True
 while y:
     entrada_avaliacao = input("\n1-Responder Avaliação"
-                            "\n"
-                            "\n2-Sair"
-                            "\n"
-                            "\n"
-                            "Escolha sua opção: ")
+                              "\n"
+                              "\n2-Sair"
+                              "\n"
+                              "\n"
+                              "Escolha sua opção: ")
 
     if entrada_avaliacao == '1':
-        print("\nOpção 1 selecionada: Responder avaliação.\n")
+        print("\nOpção 1 selecionada: Responder avaliação.")
 
-    # Definindo as perguntas como uma lista de dicionários
-
-    # Verifica se o arquivo JSON já existe
+        # Verifica se o arquivo JSON já existe
         if os.path.exists(local_perguntas):
-            # Se existir, carrega os usuários existentes
+            # Se existir, carrega as perguntas existentes
             with open(local_perguntas, 'r') as arquivo:
                 perguntas = json.load(arquivo)
         else:
             # Se não existir, cria uma lista vazia
             perguntas = []
 
+        # Lista para armazenar as respostas
+        respostas = []
+
         # Função para obter a resposta do usuário como um número inteiro entre 1 e 5
         def obter_resposta():
             while True:
                 try:
-                    resposta = int(input("\nPor favor, avalie de 1 a 5: \n"))
+                    resposta = int(input("\nPor favor, avalie de 1 a 5: "))
                     if resposta < 1 or resposta > 5:
                         raise ValueError
                     break
                 except ValueError:
-                    print("\nPor favor, insira um número inteiro entre 1 e 5.")
+                    print("\nNúmero Invalido\nPor favor, insira um número inteiro entre 1 e 5.")
             return resposta
 
         # Loop para obter as respostas do participante
-        x = 1
         for pergunta in perguntas:
             print(pergunta["descricao"])
-            resposta = {'ip': str(x),
+            resposta = {'ip': str(pergunta["ip"]),
                         'resp': str(obter_resposta())}
-            x = x + 1
-            if os.path.exists(local_resposta):
-                # Se existir, carrega os usuários existentes
-                with open(local_resposta, 'r') as arquivo:
-                    respostas = json.load(arquivo)
-            else:
-                # Se não existir, cria uma lista vazia
-                respostas = []
             respostas.append(resposta)
 
         # Salvar as respostas em um arquivo JSON
@@ -67,3 +59,5 @@ while y:
         y = False
     else:
         print("\nOpção inválida.\nPor favor, escolha uma opção válida.\n")
+
+print("Fim do programa.\n")
