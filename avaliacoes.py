8424e26e39f7df54a0178cb2a132767bac069a27
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-04-16 16:42:16 -0300

    construção avaliacao de grupo

---
+++ avaliacoes.py
@@ -0,0 +1,159 @@
+import json
+import os
+
+def autoAvaliacao(id_usuario):
+
+    local_perguntas = 'data/perguntas_autoAvaliacao.json'
+    local_resposta = 'data/respostas_autoAvaliacao.json'
+    local_identificacao = 'data/usuarios.json'
+
+    #loop prompt
+
+    # Verifica se o arquivo JSON já existe
+    if os.path.exists(local_perguntas):
+        # Se existir, carrega as perguntas existentes
+        with open(local_perguntas, 'r', encoding="UTF-8") as arquivo:
+            perguntas = json.load(arquivo)
+    else:
+        # Se não existir, cria uma lista vazia
+        perguntas = []
+
+
+
+    # verifica se o json das respostas ja existe (se alguem ja respondeu ou não)    
+    if os.path.exists(local_resposta):
+            # abre o arquivo JSON
+            with open(local_resposta, 'r', encoding="UTF-8") as resps:
+                respostas = json.load(resps)    
+    # Lista para armazenar as respostas
+    else:
+        respostas = []
+
+
+    # Função para obter a resposta do usuário como um número inteiro entre 1 e 5
+    def obter_resposta():
+        while True:
+            try:
+                resposta = int(input("\nPor favor, avalie de 1 a 5: "))
+                if resposta < 1 or resposta > 5:
+                    raise ValueError
+                break
+            except ValueError:
+                print(
+                    "\nNúmero Invalido\nPor favor, insira um número inteiro entre 1 e 5.")
+        return resposta
+
+
+    # função para retornar o maior id das respostas
+    def getNextIdResp():
+        if os.path.exists(local_resposta):
+            # abre o arquivo JSON
+            with open(local_resposta, 'r', encoding="UTF-8") as resps:
+                respostas = json.load(resps)
+            ids = []
+            for resposta in respostas:
+                ids.append(int(resposta['id_resposta']))
+            return max(ids) + 1
+        else:
+            return 0    
+    # Loop para obter as respostas do participante
+    for pergunta in perguntas:
+        print(pergunta["descricao"])
+        resposta = {'id_resposta': getNextIdResp(),
+                    'ip': str(pergunta["ip"]),
+                    'resp': str(obter_resposta()),
+                    'id_usuario': id_usuario}
+        respostas.append(resposta)  
+        # Salvar as respostas em um arquivo JSON
+        with open(local_resposta, "w") as arquivo:
+            json.dump(respostas, arquivo, indent=5) 
+    print("\nRespostas salvas no sistema!.\n")
+    y = False
+    
+def avaliacao(id_usuario, id_time):
+    local_perguntas = 'data/perguntas_grupo_avaliacao.json'
+    local_resposta = 'data/respostas_grupoAvaliacao.json'
+    local_identificacao = 'data/usuarios.json'
+
+            
+    # Função para obter a resposta do usuário como um número inteiro entre 1 e 5
+    def obter_resposta():
+        while True:
+            try:
+                resposta = int(input("\nPor favor, avalie de 1 a 5: "))
+                if resposta < 1 or resposta > 5:
+                    raise ValueError
+                break
+            except ValueError:
+                print(
+                    "\nNúmero Invalido\nPor favor, insira um número inteiro entre 1 e 5.")
+        return resposta
+
+
+    # função para retornar o maior id das respostas
+    def getNextIdResp():
+        if os.path.exists(local_resposta):
+            # abre o arquivo JSON
+            with open(local_resposta, 'r', encoding="UTF-8") as resps:
+                respostas = json.load(resps)
+            ids = []
+            for resposta in respostas:
+                ids.append(int(resposta['id_resposta']))
+            return max(ids) + 1
+        else:
+            return 0
+        
+        
+    
+    time_usuarios = []
+    # Verifica se o arquivo JSON já existe
+    if os.path.exists(local_identificacao):
+        # Se existir, carrega as perguntas existentes
+        with open(local_identificacao, 'r', encoding="UTF-8") as usu:
+            usuarios = json.load(usu)
+    else:
+        # Se não existir, cria uma lista vazia
+        usuarios = []
+    for usuario in usuarios:
+        if usuario['id_time'] == id_time and usuario['id_usuario'] != id_usuario:
+            time_usuarios.append(usuario)
+    
+    #para cada usuario pertencente ao time
+    for usuario in time_usuarios:
+        
+        print('\n Em relação a(ao) integrante {}, responda: \n'.format(usuario['identificacao']))
+    
+        # Verifica se o arquivo JSON já existe
+        if os.path.exists(local_perguntas):
+            # Se existir, carrega as perguntas existentes
+            with open(local_perguntas, 'r', encoding="UTF-8") as arquivo:
+                perguntas = json.load(arquivo)
+        else:
+            # Se não existir, cria uma lista vazia
+            perguntas = []
+
+
+
+        # verifica se o json das respostas ja existe (se alguem ja respondeu ou não)    
+        if os.path.exists(local_resposta):
+                # abre o arquivo JSON
+                with open(local_resposta, 'r', encoding="UTF-8") as resps:
+                    respostas = json.load(resps)    
+        # Lista para armazenar as respostas
+        else:
+            respostas = []
+                    
+            
+        # Loop para obter as respostas do participante
+        for pergunta in perguntas:
+            print(pergunta["descricao"])
+            resposta = {'id_resposta': getNextIdResp(),
+                        'ip': str(pergunta["ip"]),
+                        'resp': str(obter_resposta()),
+                        'id_usuario_respondeu': id_usuario,
+                        'id_usuario_avaliado': usuario['id_usuario']}
+            respostas.append(resposta)  
+            # Salvar as respostas em um arquivo JSON
+            with open(local_resposta, "w") as arquivo:
+                json.dump(respostas, arquivo, indent=5) 
+        print("\nRespostas salvas no sistema!.\n")
---
+++ promptAvaliacao.py
@@ -0,0 +1,42 @@
+import json
+import os
+import random
+import avaliacoes
+
+local_perguntas = 'data/perguntas_autoAvaliacao.json'
+local_resposta = 'data/respostas_autoAvaliacao.json'
+local_identificacao = 'data/usuarios.json'
+
+# abre o arquivo JSON
+with open(local_identificacao, 'r', encoding="UTF-8") as arquivo:
+    usuarios = json.load(arquivo)
+
+# busca o nome e o id do usuario "logado"
+for usuario in usuarios:
+    if usuario["id_usuario"] == 0:
+        nome = usuario['identificacao']
+        id_usuario = usuario["id_usuario"]
+        id_time = usuario["id_time"]
+
+# exibe o nome do usuario
+print(f"Olá, {nome}!\nSeja muito bem vindo ao nosso Sistema de Avaliação 360º! O que deseja fazer?")
+
+
+entrada_avaliacao = input("\n1 - Responder Avaliação"
+                              "\n"
+                              "\n2 - Sair"
+                              "\n"
+                              "\n"
+                              "Escolha uma opção: ")
+
+if entrada_avaliacao == '1':
+    print("\nOpção 1 selecionada: 'Responder avaliação'")
+    
+    avaliacoes.autoAvaliacao(id_usuario)
+    avaliacoes.avaliacao(id_usuario, id_time)
+    
+elif entrada_avaliacao == '2':
+    print("\nOpção 2 selecionada: 'Sair'\n\nSaindo...\n")
+    y = False
+else:
+    print("\nOpção inválida.\nPor favor, escolha uma opção válida.\n")
