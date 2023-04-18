c4de186e2ca9e57fb9569422c4399c4d6e8db550
Davidson Sousa <127262750+DavidsonSous@users.noreply.github.com>
2023-04-18 16:56:22 -0300

    Editado os times, bem como correções de bugs e etc

--- times/editTime.py
+++ times/editTime.py
@@ -1,57 +1,64 @@
 import json
-#funcao para editar time
+
+
+# funcao para editar time
 def editTime():
-        arqv_turma = open('./data/turmas.json')
-        ler_arqv_turma = json.load(arqv_turma)
+    arqv_turma = open("./data/turmas.json")
+    ler_arqv_turma = json.load(arqv_turma)
 
-        print('\nTurmas:')
+    print("\nTurmas:")
 
-        for turma in ler_arqv_turma:
-            print(turma.get('id_turma'), '-', turma.get('identificacao'))
+    for turma in ler_arqv_turma:
+        print(turma.get("id_turma"), "-", turma.get("identificacao"))
 
-        id_turma = int(input('\nDigite qual turma deseja visualizar os times: '))
+    id_turma = int(input("\nDigite qual turma deseja visualizar os times: "))
 
-        arqv_time = open('data/times.json')
-        ler_arqv_time = json.load(arqv_time)  # .load() - leitura do arqvo
+    arqv_time = open("data/times.json", encoding="UTF-8")
+    ler_arqv_time = json.load(arqv_time)  # .load() - leitura do arqvo
 
-        print('\nTimes:')
-        x = False
-        
-        for time in ler_arqv_time:
-            if time.get('id_turma') == id_turma:
-                        print(time.get('id_time'), '-', time.get('identificacao'))
-                        x = True
-        
-        if x == False:
-                   print('Turma inválida!')
+    print("\nTimes:")
+    x = False
 
-        id_time = int(input('\nDigite qual time deseja editar: '))
+    for time in ler_arqv_time:
+        if time.get("id_turma") == id_turma:
+            print(time.get("id_time"), "-", time.get("identificacao"))
+            x = True
 
-            # elif time.get('id_turma') == False:
-            #     print('Turma inválida!')
+    if x == False:
+        print("Turma inválida!")
 
-        id_existentes = [ time.get("id_time") for time in ler_arqv_time ]
-        if id_time not in id_existentes:
-            print("Time invalido")
-        else:
-            for time in ler_arqv_time:
-                    if time.get('id_time') == id_time:  # get() - metododo p/ puxar o valor que eu quero
-                        print('\nInsira as novas informações\n')
-                        novo_nome_time = input('Nome do time: ')
-                        novo_num_intg = input('Número máximo de integrantes: ')
-                        time['identificacao'] = novo_nome_time
-                        time['nr_integrantes'] = novo_num_intg
-                        print('\n--------------------------')
-                        print(time.get('id_time'), '-', time.get('identificacao'))
-                        print('Máximo de integrantes: ', time.get('nr_integrantes'))
-                        print('Turma: ', time.get('id_turma'))
-                        print('--------------------------')
-                        print('\nEdição realizada com sucesso!')
-                    # elif time.get('id_time') :
+    id_time = int(input("\nDigite qual time deseja editar: "))
 
-        caminho = './data/times.json'
+    id_existentes = [
+        time.get("id_time")
+        for time in ler_arqv_time
+        if time.get("id_turma") == id_turma
+    ]
+    if id_time not in id_existentes:
+        print("Time inválido!")
+        editTime()
+    else:
+        ind_time_alvo = id_existentes.index(id_time)
+        print("\nInsira as novas informações\n")
+        ler_arqv_time[ind_time_alvo]["identificacao"] = input("Nome do time: ")
+        ler_arqv_time[ind_time_alvo]["nr_integrantes"] = input(
+            "Número máximo de integrantes: "
+        )
+        print("\n--------------------------")
+        print(
+            ler_arqv_time[ind_time_alvo]["id_time"],
+            "-",
+            ler_arqv_time[ind_time_alvo]["identificacao"],
+        )
+        print("Máximo de integrantes: ", ler_arqv_time[ind_time_alvo]["nr_integrantes"])
+        print("Turma: ", ler_arqv_time[ind_time_alvo]["id_turma"])
+        print("--------------------------")
+        print("\nEdição realizada com sucesso!")
+    # elif time.get('id_time') :
 
-        with open(caminho, 'w') as output:
-            json.dump(ler_arqv_time, output)
+    caminho = "./data/times.json"
 
-        output.close()
+    with open(caminho, "w") as output:
+        json.dump(ler_arqv_time, output)
+
+    output.close()
