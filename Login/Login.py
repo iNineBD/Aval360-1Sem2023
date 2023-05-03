7293c73de03b21d297924fa7c7c60a78b094586a
João Matheus Lamão <joaomatheuslamao9@gmail.com>
2023-05-02 21:42:30 -0300

    finalização Login

--- Login/Login.py
+++ Login/Login.py
@@ -4,7 +4,8 @@ import hashlib
 from Administrador.prompt_main_adm import promptMainAdm
 from Aluno.promptAvaliacao import prompt_avaliacao
 
-caminho_usuarioa = "./data/usuarios.json"
+caminho_usuarios = "./data/usuarios.json"
+caminho_times = "./data/times.json"
 
 class Login:
     
@@ -12,7 +13,7 @@ class Login:
     def logar(cpf, senha):
         
         #abre o arquivo json dos usuarios
-        with open(caminho_usuarioa, 'r') as usuarios:
+        with open(caminho_usuarios, 'r', encoding="UTF-8") as usuarios:
             usuarios = json.load(usuarios)
         
         for usuario in usuarios:
@@ -27,14 +28,71 @@ class Login:
                 if usu_ok['tp_usu'] == 0:
                     promptMainAdm()
                     
-                elif usu_ok['tp_usu'] == 1:
+                elif usu_ok['tp_usu'] == 1 and type(usu_ok["id_time"]) is int:
                     prompt_avaliacao(usu_ok['id_usuario'])
+                    
+                elif usu_ok['tp_usu'] == 1 and type(usu_ok["id_time"]) is str:
+                    index_usuario = usuarios.index(usu_ok)
+                    Login.setTimeUsu(usu_ok['id_usuario'], index_usuario)
+                    
+                    prompt_avaliacao(usu_ok["id_usuario"])
+                    
                     
             else:
                 # se n estiver ok, erro
                 raise ValueError
         except ValueError:
             print('Credenciais inválidas!!! Tente novamente.')
+            
+            
+    def setTimeUsu(id_usu, index_usu):
+        while True:
+            try:
+                cod =  input('digite o código de acesso: ')
+                if not Login.verificaCod(cod):
+                    raise ValueError
+                else:
+                    id_time = Login.verificaCod(cod)
+                    break
+            except:
+                print("Código de acesso inválido! Tente novamente!")
+        
+        with open(caminho_usuarios, encoding="UTF-8") as usuarios:
+            usuarios = json.load(usuarios)
+        
+        usuario = usuarios[index_usu]
+        
+        new_usu = {
+            "id_usuario": usuario["id_usuario"],
+            "identificacao": usuario["identificacao"],
+            "senha": usuario["senha"],
+            "cpf": usuario["cpf"],
+            "tp_usu": 1,
+            "dt_nasc": usuario["dt_nasc"],
+            "id_time": id_time
+            }
+        
+        del(usuarios[index_usu])
+        
+        usuarios.append(new_usu)
+        
+        with open(caminho_usuarios, 'w') as usu:
+            json.dump(usuarios, usu)
+            
+        
+    def verificaCod(cod):
+        with open(caminho_times) as times:
+            times = json.load(times)
+        
+        for time in times:
+            if time["cod_acesso"] == cod:
+                id_time = time['id_time']
+                break
+        if "id_time" in locals():
+            return id_time
+        else:
+            return False
+        
             
     def criptografar_senha(senha):
         # Cria um objeto de hash usando o algoritmo SHA-256
