dd924943f009adc7e71bacea8ea69c970cb4cb17
João Matheus Lamão <77554165+JoaoMatheusLamao@users.noreply.github.com>
2023-05-15 20:16:12 -0300

    Update prompt_turmas.py
    design prompt Turmas

--- Administrador/turmas/prompt_turmas.py
+++ Administrador/turmas/prompt_turmas.py
@@ -1,37 +1,38 @@
 from Administrador.turmas.ctrl_turmas import Turmas
+from time import sleep
 import os
 
 def ctrl_turmas():
     while True:
         print('')
-        print("Controle de Turmas!!!")
+        print("\033[32;1;4mCONTROLE DE TURMAS\033[m")
         print('')
         while True:
             try:
-                print("Escolha uma opção: \n1 - Criar nova turma\n2 - Visualizar turmas\n3 - Editar turma\n4 - Excluir turma\n0 - Voltar\n")
-                op = int(input("Digite aqui: "))
+                print("\033[3mCerto, escolha uma opção\033[m:\n\n1 - Criar Turma\n2 - Visualizar Turma\n3 - Editar Turma\n4 - Excluir Turma\n0 - Voltar\n")
+                op = int(input("\033[3;33;1mDigite aqui\033[m: "))
                 break
             except ValueError:
-                print('\nOpção inválida! Tente novamente!\n')   
+                print('\n\033[1;31mOPÇÃO INVÁLIDA!\nTente novamente...\033[m\n')
         
         if op == 1:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("\nCriar nova turma!!!")
+            print('\n\033[3;1mVocê escolheu a opção:\033[m \033[4;33m"Criar Turma"\033[m\n')
             print(Turmas.createTurmas())
             
         elif op == 2:
             
-            print("\nVisualizando turmas!!!")
+            print('\n\033[3;1mVocê escolheu a opção:\033[m \033[4;33m"Visualizar Turma"\033[m\n')
             Turmas.listAllTurmas()
             
         elif op == 3:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("\nEditar turma!!!\n")
+            print('\n\033[3;1mVocê escolheu a opção:\033[m \033[4;33m"Editar Turma"\033[m\n')
             print(Turmas.editTurmas())
             
         elif op == 4:
             os.system('cls' if os.name == 'nt' else 'clear')
-            print("\nExcluír turma!!!")
+            print('\n\033[3;1mVocê escolheu a opção:\033[m \033[4;33m"Excluír Turma"\033[m\n')
             print(Turmas.delTurma())
         
         elif op == 0:
@@ -39,5 +40,4 @@ def ctrl_turmas():
             break
             
         else:
-            print('\nOpção inválida! Tente novamente!')
-        print("----------------------------------------------------")
+            print('\n\033[1;31mOPÇÃO INVÁLIDA!\nTente novamente...\033[m\n')
